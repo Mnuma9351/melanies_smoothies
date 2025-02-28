88aa4f43381c5ad7e0c9fe27cf3b1c75695f7e10
Mnuma9351 <mayuko.numazaki@g.softbank.co.jp>
2025-02-28 13:41:57 +0900

    Update streamlit_app.py
    fruit_chosen周り修正

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
@@ -28,7 +29,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-
+        st.subheader(fruit_chosen + 'Nutrition Information')
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+    
     #st.write(ingredients_string)
 
     my_insert_stmt = """ insert into smoothies.public.orders (ingredients, name_on_order)
@@ -44,8 +48,3 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-# New section to display smoothiefoot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-#st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
